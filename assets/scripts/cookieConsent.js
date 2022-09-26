6f5b52fe635f29183ccb5098c292c21b08a1fe42
delphidabbler <5164283+delphidabbler@users.noreply.github.com>
2022-09-26 16:21:49 +0100

    Replace 3rd party cookie consent with custom code
    Remove closed source code from Osano
    Add new custom written open source code

---
+++ assets/scripts/cookieConsent.js
@@ -0,0 +1,38 @@
+/*
+ * Cookie consent script
+ * (C) 2022, Peter Johnson | MIT License | Part of github.com/delphidabbler/delphidabbler.github.io
+ *
+ * Requires cookies.js
+ *
+ * To style this item provide CSS for:
+ *    #cookie-popup (bounding <div>)
+ *    p.cookie-msg (message string <p>)
+ *    p.cookie-btn (<p> containing close button)
+ *    p.cookie-btn button (close <button> itself)
+ */
+
+var CookieConsent = {
+    exec: function() {
+        let cookieName = 'dd_inhibit_cookie_consent';
+        if ( Cookie.read(cookieName) ) {
+            return;
+        }
+        $(document.body).prepend(
+            '<div id="cookie-popup" class="container-fluid">'
+            + '<p class="cookie-msg">This website uses a minimum number of cookies. '
+            + 'There are no trackers or advertising cookies. '
+            + '<a href="/cookies">More info</a>.'
+            + '</p>'
+            + '<p class="cookie-btn"><button id="cookie-close-btn">Dismiss</button></p>'
+            + '</div>'
+        );
+        $('#cookie-close-btn').click(function() {
+            Cookie.write(cookieName, "dismissed", 91);  // expires in apx 3 mths
+            $('#cookie-popup').hide();
+        });
+    }
+}
+
+$( function() {
+    CookieConsent.exec();
+});
