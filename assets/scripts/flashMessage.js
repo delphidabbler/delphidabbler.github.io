9b967df51212f4656ae48fc95c4d9f12af09464a
delphidabbler <5164283+delphidabbler@users.noreply.github.com>
2022-09-28 11:50:56 +0100

    Add license & update comments in flashMessage.js

--- assets/scripts/flashMessage.js
+++ assets/scripts/flashMessage.js
@@ -1,18 +1,23 @@
 /**
- * Enables "flash" messages to persist their display state. Once user has
- * the close button a cookie is set that ensures the flash is not displayed
- * again.
+ * Flash message script.
+ *
+ * Enables "flash" messages to persist their display state. Once user has clicked the close button
+ * a cookie is set that ensures the flash is not displayed again (for 1 year).
+ *
+ * (C) 2022, Peter Johnson | MIT License | Part of github.com/delphidabbler/delphidabbler.github.io
  */
+
 
 $( function() {
     const flash = $("#flash-popup");
     if ( flash.length === 0 ) {
+        // there is no flash message in document: nothing to do
         return;
     }
     const id = flash.data('flash-id');
     const cookieName = 'dd_flash_' + id;
     flash.on('closed.bs.alert', function () {
-        // Set cookie for 1yr: can't set for more per EU regs
+        // set cookie for 1yr: can't set for more per EU regs
         Cookie.set(cookieName, "dismissed", 365);
     });
     if ( ! Cookie.isSet(cookieName) ) {
